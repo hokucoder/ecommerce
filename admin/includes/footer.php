8f07176e73e6b2d20a1235ac63bb3804655349dc
hoku <99589772+hokucoder@users.noreply.github.com>
2022-02-13 18:40:34 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div> <center>
+     <strong>Developed by: <a href="https://web.facebook.com/rickyhatesthanos">Ricky Gumanad</a></strong>
+</footer>
